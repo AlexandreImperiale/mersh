da6d51ce0e82f116c96d4896589e11ee99ba042d
IMPERIALE <alexandre.imperiale@cea.fr>
2018-03-14 13:37:12 +0100

    Removing irrelevant documentation.

--- docs/Post_10_08_2016/Coord2d-snippet.rs
+++
@@ -1,6 +0,0 @@
-pub struct Coord2d {
-    /// First coordinate of geometrical element.
-    pub x: f64,
-    /// Second coordinate of geometrical element.
-    pub y: f64,
-}
--- docs/Post_10_08_2016/Pnt2d-Vec2d-snippet.rs
+++
@@ -1,9 +0,0 @@
-pub struct Pnt2d {
-    /// Coordinates associated to the point.
-    pub coords: Coord2d,
-}
-
-pub struct Vec2d {
-    /// Coordinates associated to the vector.
-    pub coords: Coord2d,
-}
